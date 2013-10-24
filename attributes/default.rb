0b90adec5c44e6d88d63cc3996bd30fc868db577
Doug Bible <dbible@scrippsnetworks.com>
2013-10-24 14:16:06 -0400

    Chef Training apache cookbook - initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['apache']['sites']['clowns'] = { "port" => 80, "image" => "http://reorg.co/wordpress/wp-content/uploads/developers-designers-managers.jpg.scaled1000.jpg"}
+default['apache']['sites']['bears'] = { "port" => 81, "image" => "http://reorg.co/wordpress/wp-content/uploads/developers-designers-managers.jpg.scaled1000.jpg"}
+default['apache']['sites']['ponies'] = { "port" => 83, "image" => "http://reorg.co/wordpress/wp-content/uploads/developers-designers-managers.jpg.scaled1000.jpg"}
